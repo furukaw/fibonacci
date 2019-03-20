5257b51e5b55ae132bcc7645b618b2a8f3a9e978
Tsukino Furukawa <tsukino95@gmail.com>
2019-03-20 08:09:57 +0000

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+function fib(n) {  // fib(0) = 0 としてフィボナッチ数列の n 番目の数を返す
+    if (n === 0) {  // fib(0) = 0
+        return 0;
+    } else if (n === 1) {  // fib(1) = 1
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);  // fib(n) = fib (n - 1) + fib(n - 2) (n > 1)
+}
+const length = 40;  // fib(0) から fib(40) を順番に出力
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
